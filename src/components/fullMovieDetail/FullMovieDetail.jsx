5541f517aab800db0f970026df9484582f388c7e
Baljinder-Singh <baljindersingh987209@gmail.com>
2026-03-23 20:57:43 +0530

    refactor: add useSelector import to FullMovieDetail component

--- src/components/fullMovieDetail/FullMovieDetail.jsx
+++ src/components/fullMovieDetail/FullMovieDetail.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { API_OPTION } from '../../utils/constentsForMovieApi';
 import {
   fetchTrailerOfMovie,
